dde155bae0080cc8a1f89b66a8b54c20223eab67
Ayush Kumar <aryaayushgithub0208@gmail.com>
2020-09-29 19:43:55 +0530

    Pattern_10

---
+++ Pattern_10.java
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+package Programs;
+import java.util.Scanner;
+public class Pattern_10 {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.print("Enter the value of n :");
+		int n = obj.nextInt();
+		System.out.println("# ");
+		for(int i = 0 ; i<=n-2 ;i++) {
+			System.out.print("# ");
+			for(int j = 1 ; j<=i ;j++) {
+				System.out.print("  ");
+			}
+			System.out.print("# ");
+			System.out.println();
+		}
+		for(int g = 1; g<=n+1 ;g++) {
+			System.out.print("# ");
+		}
+
+	} 
+
+}
+=======
+package Programs;
+import java.util.Scanner;
+public class Pattern_10 {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.print("Enter the value of n :");
+		int n = obj.nextInt();
+		System.out.println("# ");
+		for(int i = 0 ; i<=n-2 ;i++) {
+			System.out.print("# ");
+			for(int j = 1 ; j<=i ;j++) {
+				System.out.print("  ");
+			}
+			System.out.print("# ");
+			System.out.println();
+		}
+		for(int g = 1; g<=n+1 ;g++) {
+			System.out.print("# ");
+		}
+
+	} 
+
+}
+>>>>>>> b996365d25a2f16d0e5a67d91ec43bfaa5ba752e
